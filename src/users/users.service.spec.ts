d29f580f7a20b69470c7d9e96a27e23aa1bed507
hojoon <ujuj0202@gmail.com>
2021-02-08 18:13:21 +0900

    mock fn,repository

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,43 @@
 import { Test } from "@nestjs/testing"
+import { getRepositoryToken } from "@nestjs/typeorm"
+import { EmailService } from "src/email/email.service"
+import { JwtService } from "src/jwt/jwt.service"
+import { User } from "./entities/user.entity"
+import { Verification } from "./entities/verification.entity"
 import { UsersService } from "./users.service"
 
+
+const mockRepository = {
+    findOne:jest.fn(),
+    save:jest.fn(),
+    create:jest.fn(),
+    delete:jest.fn(),
+}
+const mockJwtService = {
+    sign:jest.fn(),
+    verify:jest.fn(),
+}
+const mockEmailService ={
+    sendVerificationEmail:jest.fn(),
+}
 describe("User Service", ()=>{
     let service:UsersService
 
     beforeAll(async () => {
         const module = await Test.createTestingModule({
             providers:[
-                UsersService,
+                UsersService, {
+                    provide: getRepositoryToken(User), useValue:mockRepository
+                },
+                {
+                    provide: getRepositoryToken(Verification), useValue:mockRepository
+                },
+                {
+                    provide: JwtService, useValue:mockJwtService
+                },
+                {
+                    provide: EmailService, useValue:mockEmailService
+                }
             ]
         }).compile();
         service = module.get<UsersService>(UsersService)
