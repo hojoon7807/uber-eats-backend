b265a78d6a22689935b80b467c1692834973ce9f
hojoon <ujuj0202@gmail.com>
2021-02-08 20:08:51 +0900

    first test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from "@nestjs/testing"
 import { getRepositoryToken } from "@nestjs/typeorm"
 import { EmailService } from "src/email/email.service"
 import { JwtService } from "src/jwt/jwt.service"
+import { Repository } from "typeorm"
 import { User } from "./entities/user.entity"
 import { Verification } from "./entities/verification.entity"
 import { UsersService } from "./users.service"
@@ -20,9 +21,12 @@ const mockJwtService = {
 const mockEmailService ={
     sendVerificationEmail:jest.fn(),
 }
+type MockRepository<T= any> = Partial<Record<keyof Repository<T>,jest.Mock>>;
+
+
 describe("User Service", ()=>{
     let service:UsersService
-
+    let usersRepository:MockRepository<User>
     beforeAll(async () => {
         const module = await Test.createTestingModule({
             providers:[
@@ -41,11 +45,27 @@ describe("User Service", ()=>{
             ]
         }).compile();
         service = module.get<UsersService>(UsersService)
+        usersRepository = module.get(getRepositoryToken(User))
     })
+    
     it("be define",()=>{
         expect(service).toBeDefined();
     })
 
+    describe("createAccount",()=>{
+        it("should fail if user exist", async ()=>{
+            usersRepository.findOne.mockResolvedValue({
+                id:1,
+                email:"test@gmail.com"
+            })
+            const result = await service.createAccount({
+                email:"",
+                password:"",
+                role:0
+            })
+            expect(result).toMatchObject({ok:false, error:"There is a user with that email already"}})
+        })
+    })
     it.todo("createAccount")
     it.todo("login")
     it.todo("findById")
