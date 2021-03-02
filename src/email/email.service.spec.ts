24a3a2e3e1ab6caa6f4c1cac4fbbec528f187a4d
hojoon <ujuj0202@gmail.com>
2021-03-02 19:40:08 +0900

    email service sendEmail fn use jest spyOn test

--- src/email/email.service.spec.ts
+++ src/email/email.service.spec.ts
@@ -1,13 +1,12 @@
 import { Test } from "@nestjs/testing";
+import got from "got";
+import * as FormData from "form-data";
 import { CONFIG_OPTIONS } from "src/common/common.constant";
 import { EmailService } from "./email.service"
 
 jest.mock('got');
-jest.mock('form-data',()=>{
-    return{
-        append:jest.fn(()=>{})
-    }
-})
+jest.mock('form-data');
+const TEST_DOMAIN = 'test-domain'
 describe('email service',()=>{
     let service:EmailService;
     beforeEach(async ()=>{
@@ -17,7 +16,7 @@ describe('email service',()=>{
                     provide:CONFIG_OPTIONS,
                     useValue:{
                         apiKey:'test-apiKey',
-                        domain:'test-domain',
+                        domain:TEST_DOMAIN,
                         fromEmail:'test-fromEmail',
                     }
                 }
@@ -34,7 +33,7 @@ describe('email service',()=>{
                 email:'email',
                 code:'code'
             };
-            jest.spyOn(service,'sendEmail').mockImplementation(async () => {})
+            jest.spyOn(service,'sendEmail').mockImplementation(async () => true)
             service.sendVerificationEmail(sendVerificationEmailArgs.email,sendVerificationEmailArgs.code)
             expect(service.sendEmail).toHaveBeenCalledTimes(1);
             expect(service.sendEmail).toHaveBeenCalledWith("Verify your Email", "d",
@@ -44,8 +43,20 @@ describe('email service',()=>{
     })
     
     describe('sendEmail',()=>{
-        it('should be send email',()=>{
-            service.sendEmail('','',[]);
+        it('should be send email',async ()=>{
+            const result = await service.sendEmail('','',[{ key: 'attr', value: 'attrValue' }]);
+            const formSpy = jest.spyOn(FormData.prototype,'append');
+            expect(formSpy).toHaveBeenCalledTimes(5);
+            expect(got.post).toHaveBeenCalledTimes(1);
+            expect(got.post).toHaveBeenCalledWith(`https://api.mailgun.net/v3/${TEST_DOMAIN}/messages`,expect.any(Object));
+            expect(result).toEqual(true);
+            });
+        it('should be fail',async()=>{
+            jest.spyOn(got,'post').mockImplementation(()=>{
+                throw new Error
+            });
+            const result = await service.sendEmail('','',[]);
+            expect(result).toEqual(false);
+        })
         })
     })
-})
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -3,13 +3,14 @@ import got from "got";
 import * as FormData from "form-data";
 import { CONFIG_OPTIONS } from 'src/common/common.constant';
 import { EmailModuleOptions, EmailVar } from './email.interfaces';
+import { BooleanSchema } from 'joi';
 
 @Injectable()
 export class EmailService {
     constructor(
         @Inject(CONFIG_OPTIONS) private readonly options:EmailModuleOptions,
     ){}
-    async sendEmail(subject:string, template:string, emailVars:EmailVar[]){
+    async sendEmail(subject:string, template:string, emailVars:EmailVar[]):Promise<Boolean>{
         const form = new FormData()
         form.append('from',`hojoon from uber eats <mailgun@${this.options.domain}>`)
         form.append('to',`ujuj0202@gmail.com`)
@@ -17,17 +18,17 @@ export class EmailService {
         form.append('template',template)
         emailVars.forEach(eVar=>{form.append(`v:${eVar.key}`,eVar.value)}); 
         try{
-            await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`,{
-                method:'POST',
+            await got.post(`https://api.mailgun.net/v3/${this.options.domain}/messages`,{
                 headers:{
                     Authorization:`Basic ${Buffer.from(
                         `api:${this.options.apiKey}`,
                     ).toString("base64")}`
                 },
                 body:form,
-            })
+            });
+            return true
         }catch(error){
-            
+            return false
         }
     }
     sendVerificationEmail(email:string,code:string){
